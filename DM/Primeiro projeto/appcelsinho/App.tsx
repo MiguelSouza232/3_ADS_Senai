ce4e6930b5f443cfa7e7b67af2527e84f1b636eb
MiguelSouza232 <miguelsouza0204@gmail.com>
2026-03-20 22:05:27 -0300

    .

--- DM/Primeiro projeto/appcelsinho/App.tsx
+++ DM/Primeiro projeto/appcelsinho/App.tsx
@@ -1,36 +1,49 @@
-//Menu
+import React from "react";
+
+// Navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-//MENU SOBRE
+// Ícones
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+// Tela
 import telasSobre from "./telas/Sobre";
 
-//MENU - BOTTOM TABS
-const Tab = createBottomTabNavigator();
+// Tipagem das rotas
+type RootTabParamList = {
+  Sobre: undefined;
+  Servicos: undefined;
+  Perfil: undefined;
+};
+
+const Tab = createBottomTabNavigator<RootTabParamList>();
 
 function Menu() {
   return (
     <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarIcon({focused, color, size}) => ({
-          let iconName: any;
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: string;
+
           if (route.name === "Sobre") {
             iconName = focused ? "home" : "home-outline";
           } else if (route.name === "Servicos") {
             iconName = focused ? "bag" : "bag-outline";
-          } else if (route.name === "Perfil") {
+          } else {
             iconName = focused ? "person" : "person-outline";
           }
-        }),
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
         headerShown: false,
         tabBarActiveTintColor: "purple",
         tabBarInactiveTintColor: "gray",
       })}
     >
       <Tab.Screen name="Sobre" component={telasSobre} />
-      <Tab.Screen name="Sobre1" component={telasSobre} />
-      <Tab.Screen name="Sobre2" component={telasSobre} />
-      <Tab.Screen name="Sobre3" component={telasSobre} />
+      <Tab.Screen name="Servicos" component={telasSobre} />
+      <Tab.Screen name="Perfil" component={telasSobre} />
     </Tab.Navigator>
   );
 }
--- DM/Primeiro projeto/appcelsinho/telas/Sobre.tsx
+++ DM/Primeiro projeto/appcelsinho/telas/Sobre.tsx
@@ -1,49 +1,41 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import { VideoView, useVideoPlayer } from "expo-video";
-
-//Menu
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-//MENU - BOTTOM TABS
-const Tab = createBottomTabNavigator();
-
-function Menu() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Sobre" component={Sobre} />
-      <Tab.Screen name="Home" component={App} />
-    </Tab.Navigator>
-  );
-}
+import React from "react";
 
 export default function Sobre() {
   const player = useVideoPlayer(
     "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
     (player) => {
       player.loop = true;
-      // player.play();
     },
   );
 
   return (
-    <ScrollView style={styles.container}>
-      <Image
-        source={require("../assets/SubLogoCelsinho.png")}
-        resizeMode="contain"
-        style={styles.logo}
-      />
-      <Text style={styles.text}>APP Dj Celsinho</Text>
-      <Image
-        source={require("../assets/Contrate.png")}
-        resizeMode="contain"
-        style={styles.imagem}
-      />
-      <StatusBar style="light" animated />
+    <>
+      <ScrollView style={styles.container}>
+        <Image
+          source={require("../assets/SubLogoCelsinho.png")}
+          resizeMode="contain"
+          style={styles.logo}
+        />
 
-      <VideoView player={player} style={styles.video} allowsPictureInPicture />
-    </ScrollView>
+        <Text style={styles.text}>APP Dj Celsinho</Text>
+
+        <Image
+          source={require("../assets/Contrate.png")}
+          resizeMode="contain"
+          style={styles.imagem}
+        />
+
+        <VideoView
+          player={player}
+          style={styles.video}
+          allowsPictureInPicture
+        />
+      </ScrollView>
+      <StatusBar style="light" animated />
+    </>
   );
 }
 
@@ -72,8 +64,8 @@ const styles = StyleSheet.create({
   },
   video: {
     width: 300,
-    alignSelf: "center",
     height: 275,
     marginTop: 50,
+    alignSelf: "center",
   },
 });
