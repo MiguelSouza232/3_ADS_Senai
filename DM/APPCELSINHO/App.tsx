d590a46f81e44d2774f169f903080470b4ed9fbb
MiguelSouza232 <miguelsouza0204@gmail.com>
2026-04-24 22:04:54 -0300

    Projeto AppCelsinho atualizado

---
+++ DM/APPCELSINHO/App.tsx
@@ -0,0 +1,60 @@
+//React Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View } from "react-native";
+
+//Ícones
+import { Ionicons } from "@expo/vector-icons";
+
+//Importação da Fonte
+import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
+
+//Menu SOBRE
+import TelaSobre from "./telas/Sobre";
+
+//MENU - BOTTOM TABS
+const Tab = createBottomTabNavigator();
+
+function Menu() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: any;
+
+          if (route.name === "Sobre") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Produtos") {
+            iconName = focused ? "musical-note" : "musical-note-outline";
+          } else if (route.name === "Perfil") {
+            iconName = focused ? "person" : "person-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarActiveTintColor: "#6d24ca",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Sobre" component={TelaSobre} />
+      <Tab.Screen name="Produtos" component={TelaSobre} />
+      <Tab.Screen name="Perfil" component={TelaSobre} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  //Carrega a fonte
+  const [fonteCarregada] = useFonts({ FontePadrao: Poppins_400Regular });
+
+  //Verifica se a fonte foi carregada
+  if (!fonteCarregada) {
+    return <View />;
+  }
+
+  return (
+    <NavigationContainer>
+      <Menu />
+    </NavigationContainer>
+  );
+}
---
+++ DM/APPCELSINHO/telas/Sobre.tsx
@@ -0,0 +1,107 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, ScrollView, Image, View } from "react-native";
+import { VideoView, useVideoPlayer } from "expo-video";
+
+//Componente de Texto
+import Texto from "../componentes/Texto";
+
+export default function Sobre() {
+  //Indica o vídeo e coloca ele em loop
+  const player = useVideoPlayer(
+    require("../assets/VideoLoop.mp4"),
+    (player) => {
+      player.loop = true;
+      //player.play()
+    },
+  );
+
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("../assets/CelsinhoGregs.jpg")}
+          style={styles.backgroundImage}
+          resizeMode="contain"
+        />
+        <Image
+          source={require("../assets/LogoCelsinho.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
+
+      <Texto estiloEspecifico={styles.texto}>
+        Crescer rodeado por sons e ritmos foi algo mágico. Lembro de tardes
+        inteiras ouvindo meu pai mixar músicas, aprendendo a sentir a batida e
+        compreender como cada estilo tem sua alma. Aos poucos, fui criando minha
+        identidade sonora, explorando produções próprias e arriscando meus
+        primeiros sets.
+        {"\n"}
+        {"\n"}
+        Com o tempo, comecei a tocar em pequenas festas, eventos de amigos e até
+        mesmo ajudar meu pai em apresentações. A conexão com o público é o que
+        mais me motiva a seguir em frente.
+        {"\n"}
+        {"\n"}
+        Atualmente atuo pelas áreas de produção musical, mixagem e performance
+        ao vivo. Cada projeto é uma nova oportunidade de criar algo único e
+        emocionante. A música é minha paixão e minha missão é compartilhar essa
+        energia com o mundo.
+        {"\n"}
+      </Texto>
+
+      <Image
+        source={require("../assets/Contrate.png")}
+        style={styles.imagem}
+        resizeMode="contain"
+      />
+
+      <StatusBar style="light" animated />
+
+      <VideoView player={player} style={styles.video} allowsPictureInPicture />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#111111",
+  },
+  logoContainer: {
+    width: "100%",
+    height: 350,
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    opacity: 0.4,
+  },
+  texto: {
+    color: "white",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+  imagem: {
+    height: 350,
+    alignSelf: "center",
+    marginHorizontal: 16,
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    alignSelf: "center",
+    zIndex: 1,
+  },
+  video: {
+    width: 350,
+    height: 195,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 50,
+  },
+});
